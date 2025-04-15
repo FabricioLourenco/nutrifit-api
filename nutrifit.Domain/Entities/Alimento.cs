718354260915c95233dfe240f9035bb694c87f9d
Fabrício Borges Lourenço <lourenco.fabricio07@gmail.com>
2025-04-14 21:44:44 -0300

    Desenvolvimento do DB

---
+++ nutrifit.Domain/Entities/Alimento.cs
@@ -0,0 +1,15 @@
+﻿using nutrifit.Domain.Entities.Base;
+
+namespace Nutrifit.Domain.Entities
+{
+    public class Alimento : BaseEntity
+    {
+        public string Nome { get; set; }
+        public decimal Calorias { get; set; }
+        public decimal Proteinas { get; set; }
+        public decimal Carboidratos { get; set; }
+        public decimal Gorduras { get; set; }
+        public int CodigoIBGE { get; set; }
+        public AlimentoIbge AlimentoIbge { get; set; }
+    }
+}
---
+++ nutrifit.Domain/Entities/ItemRefeicao.cs
@@ -0,0 +1,15 @@
+﻿using nutrifit.Domain.Entities.Base;
+using Nutrifit.Domain.Enuns;
+
+namespace Nutrifit.Domain.Entities
+{
+    public class ItemRefeicao : BaseEntity
+    {      
+        public decimal Quantidade { get; set; } 
+        public UnidadeMedidaEnum UnidadeMedida { get; set; }
+        public long RefeicaoId { get; set; }
+        public long AlimentoId { get; set; }
+        public virtual Refeicao Refeicao { get; set; }
+        public virtual Alimento Alimento { get; set; }
+    }
+}
